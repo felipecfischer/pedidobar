f440d7ca96de2f80c486ad7c147d6e0e846fdb5e
Felipe Fischer <felipecorreiafischer@hotmail.com>
2019-01-02 20:28:49 -0200

    Criado projeto.

---
+++ src/main/java/br/com/pedidobar/PedidobarApplication.java
@@ -0,0 +1,13 @@
+package br.com.pedidobar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidobarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidobarApplication.class, args);
+	}
+}
+
---
+++ src/test/java/br/com/eicon/PedidobarApplicationTests.java
@@ -0,0 +1,17 @@
+package br.com.eicon;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PedidobarApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
